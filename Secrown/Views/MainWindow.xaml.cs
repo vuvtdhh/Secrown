bf3173152b74ec0c980b5eff860a41ee1b88ae31
Vũ Văn <vuvtdhh@gmail.com>
2024-08-25 01:04:23 +0700

    Update MainWindow.xaml.cs

--- Secrown/Views/MainWindow.xaml.cs
+++ Secrown/Views/MainWindow.xaml.cs
@@ -16,6 +16,27 @@ namespace Secrown
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ICommand LockCommand { get; private set; }
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        private void Minimize_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+        private void Maximize_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+        private void Restore_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
+        }
+        private void Close_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
         public MainWindow()
         {
             InitializeComponent();
