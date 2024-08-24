4c12b48a47dfa9bf3c49abf7d52e4efe830b4145
Vũ Văn <vuvtdhh@gmail.com>
2024-08-25 01:10:29 +0700

    Update MainWindow.xaml.cs
    Add ExcludeFromCapture function.

--- Secrown/Views/MainWindow.xaml.cs
+++ Secrown/Views/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -40,6 +42,25 @@ namespace Secrown
         public MainWindow()
         {
             InitializeComponent();
+        }
+        #region SetWindowDisplayAffinity
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowDisplayAffinity(IntPtr hWnd, uint dwAffinity);
+
+        private const uint WDA_NONE = 0x00000000;
+        private const uint WDA_MONITOR = 0x00000001;
+        private const uint WDA_EXCLUDEFROMCAPTURE = 0x00000011;
+
+        private void ExcludeFromCapture()
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            SetWindowDisplayAffinity(hwnd, WDA_EXCLUDEFROMCAPTURE);
+        }
+        #endregion
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ExcludeFromCapture();
         }
     }
 }
